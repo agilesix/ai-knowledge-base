c4931c5ed977d272c3e0059c62645fc7f416038f
widal001 <billy.daly@agile6.com>
2024-12-27 22:00:59 -0500

    feat: Adds GitHub action to deploy the site

--- knowledge-base/astro.config.mjs
+++ knowledge-base/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://agilesix.github.io",
+  base: "ai-knowledge-base",
   integrations: [
     starlight({
       title: "My Docs",
